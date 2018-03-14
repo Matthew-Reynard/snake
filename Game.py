bc4d806016c38e94cd9e589d67762b203fa5c375
Matthew Reynard <matthewreynard24@gmail.com>
2018-03-14 16:00:24 +0200

    Updated base game

--- Game.py
+++ Game.py
@@ -20,16 +20,19 @@ pygame.init()
 
 pygame.font.init() # you have to call this at the start, if you want to use this module.
 myfont = pygame.font.SysFont('arial', 30)
-gameOverFont = pygame.font.SysFont('arial', 64)
+gameOverFont = pygame.font.SysFont('arial', 96)
 
 #print(pygame.font.get_fonts())
 
+#Window size
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
 
+#Wrap allows snake to wrap around and not crash into the edge of the screen
 ENABLE_WRAP = True
 GAME_QUIT = False
 
+#Various RGB colours
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
@@ -37,7 +40,7 @@ red = (255,0,0)
 FPS = 15 # changing the speed of the snake
 score = 0
 MOVEMENT_SPEED = 1
-SCALE = 20
+SCALE = 20 # Block/Grid size (in pixels)
 
 #Create and Initialise Snake 
 snake = Snake(0,0,0,0)
@@ -51,29 +54,26 @@ clock = pygame.time.Clock()
 snake.tail = [(DISPLAY_WIDTH * 0.0, DISPLAY_HEIGHT * 0.0)]
 
 snake.head_img = pygame.image.load("./Images/Snake_Head.png").convert()
-snake.head_img.set_colorkey(white)
-snake.head_img = pygame.transform.scale(snake.head_img, (20, 20))
-snake.head_mask = pygame.mask.from_surface(snake.head_img)
-
-print("Red Mask is")
-print(snake.head_mask)
+snake.head_img.set_colorkey(white) # sets white to alpha
+snake.head_img = pygame.transform.scale(snake.head_img, (20, 20)) # scales it down from a 50x50 image to 20x20
+snake.head_mask = pygame.mask.from_surface(snake.head_img) # creates a mask
 
 snake.tail_img = pygame.image.load("./Images/Snake_Tail.png").convert()
 snake.tail_img.set_colorkey(white)
 snake.tail_img = pygame.transform.scale(snake.tail_img, (20, 20))
-snake.mask = [pygame.mask.from_surface(snake.tail_img)]
 
 blue_dot = pygame.image.load("./Images/BlueDot.png").convert()
 blue_dot.set_colorkey(white)
 blue_dot = pygame.transform.scale(blue_dot, (20, 20))
 blue_mask = pygame.mask.from_surface(blue_dot)
 
-#
 snake.box = [(0,0)]
 snake.box[0] = snake.head_img.get_rect()
 
 blue_rect = blue_dot.get_rect()
 
+#Load a food item into the screen at a random location
+#Needs to be updated to not load onto the snake - NB
 def makeFood():
     made = False
     rows = DISPLAY_HEIGHT / SCALE
@@ -82,34 +82,27 @@ def makeFood():
     while not made:
         myRow = random.randint(0, rows-1)
         myCol = random.randint(0, cols-1)
-        #print(myRow * SCALE, myCol * SCALE)
-        for n in snake.tail:
-            print("x: " + str(n[0]) + "   y: " + str(n[1])) 
+        #print(myRow * SCALE, myCol * SCALE) # DEBUGGING
+
+        #DEBUGGING
+        #for n in snake.tail:
+        #    print("x: " + str(n[0]) + "   y: " + str(n[1])) 
 
         blue_rect.topleft = (myCol * SCALE, myRow * SCALE)
-        made = True
-
-
-#red_rect.append((100,100))
-#dots.append(red_rect[1])
-#red_rect[1] = red_dot.get_rect()
-#red_rect[1].topleft = (100,100)
-
+        made = True # used for the updated function - NOT YET IMPLEMENTED
 
 #pygame.mouse.set_visible(False)
 
-#dots.append((2,3))
-#print(dots[1][1])
-
-#Draw the snake
+#Draws the snake
 def DrawSnake():
     #Draw head
-    gameDisplay.blit(snake.head_img, (snake.x, snake.y))
+    #gameDisplay.blit(snake.head_img, (snake.x, snake.y))
+    gameDisplay.blit(snake.head_img, snake.box[0].topleft)
     #Draw tail
-    if len(snake.tail) > 1:
-        for i in range(len(snake.tail) - 1):
+    if snake.tail_length > 0:
+        for i in range(1, snake.tail_length + 1):
             gameDisplay.blit(snake.tail_img, snake.box[i].topleft)
-            print(i)
+            #print(i) #DEBUGGING
 
 #Draw the food
 def DrawBlueDot():
@@ -144,34 +137,25 @@ def eatsFood(score):
         makeFood()
 
         snake.tail_length += 1
-        snake.tail_box.append(snake.tail_img.get_rect())
-        snake.tail_box[snake.tail_length-1].topleft = (0,0)
+        snake.box.append(snake.tail_img.get_rect()) #adds a rectangle variable to snake.box array(list)
 
         if snake.dx > 0: #RIGHT
-            snake.box.append(snake.tail_img.get_rect())
-            snake.tail.append(snake.box[len(snake.tail)-1])
-            snake.box[len(snake.tail) - 1].topleft = tuple(np.subtract(snake.box[len(snake.tail) - 2].topleft, (20, 0)))
+            snake.box[snake.tail_length].topleft = tuple(np.subtract(snake.box[snake.tail_length - 1].topleft, (20, 0)))
 
         if snake.dx < 0:#LEFT
-            snake.box.append(snake.tail_img.get_rect())
-            snake.tail.append(snake.box[len(snake.tail) - 1])
-            snake.box[len(snake.tail) - 1].topleft = tuple(np.subtract(snake.box[len(snake.tail) - 2].topleft, (-20, 0)))
+            snake.box[snake.tail_length].topleft = tuple(np.subtract(snake.box[snake.tail_length - 1].topleft, (-20, 0)))
 
         if snake.dy > 0:#DOWN
-            snake.box.append(snake.tail_img.get_rect())
-            snake.tail.append(snake.box[len(snake.tail)-1])
-            snake.box[len(snake.tail) - 1].topleft = tuple(np.subtract(snake.box[len(snake.tail) - 2].topleft, (0, 20)))
+            snake.box[snake.tail_length].topleft = tuple(np.subtract(snake.box[snake.tail_length - 1].topleft, (0, 20)))
 
         if snake.dy < 0:#UP
-            snake.box.append(snake.tail_img.get_rect())
-            snake.tail.append(snake.box[len(snake.tail)-1])
-            snake.box[len(snake.tail) - 1].topleft = tuple(np.subtract(snake.box[len(snake.tail) - 2].topleft, (0, -20)))
+            snake.box[snake.tail_length].topleft = tuple(np.subtract(snake.box[snake.tail_length - 1].topleft, (0, -20)))
 
 
 #Defines all the Players controls during the game
 def controls(GAME_OVER):
     for event in pygame.event.get():
-        #print(event)
+        #print(event) # DEBUGGING
         if event.type == pygame.QUIT:
             GAME_QUIT = True
             pygame.quit()
@@ -179,7 +163,7 @@ def controls(GAME_OVER):
             sys.exit(0) # safe backup
 
         if event.type == pygame.KEYDOWN:
-            #print(event.key)
+            #print(event.key) #DEBUGGING
 
             #Moving left
             if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and snake.dx == 0 and GAME_OVER == False:
@@ -203,112 +187,108 @@ def controls(GAME_OVER):
 
             #DEBUGGING - add to the snakes tail
             elif event.key == pygame.K_f and GAME_OVER == False: 
-                snake.box.append(snake.tail_img.get_rect())
-                snake.tail.append(snake.box[len(snake.tail)-1])
-                snake.box[len(snake.tail) - 1].topleft = tuple(np.subtract(snake.box[len(snake.tail) - 2].topleft, (20, 0)))
+                #score += 1
+                #score_text = myfont.render("Score: " + str(score), True, white)  # anti aliasing
+
+                snake.tail_length += 1
+                snake.box.append(snake.tail_img.get_rect()) #adds a rectangle variable to snake.box array(list)
+                snake.box[snake.tail_length].topleft = tuple(np.subtract(snake.box[snake.tail_length - 1].topleft, (20, 0)))
 
             #GAME OVER: Press space to try play again
             elif (event.key == pygame.K_SPACE) and GAME_OVER == True:
                 GAME_OVER = False
 
+                snake.tail_length = 0
                 snake.box.clear()
-                snake.tail.clear()
 
-                snake.box = [(0,0)]
-                snake.tail = [(0,0)]
+                snake.box = [(0,0)] # Not sure if this is neccessary
 
                 snake.box[0] = snake.head_img.get_rect()
-                print(snake.head_img.get_rect())
-                snake.box[0].topleft = (snake.tail[0][0], snake.tail[0][1])
+                snake.box[0].topleft = (0, 0)
                 makeFood()
 
+            elif (event.key == pygame.K_q) and GAME_OVER == True:
+                GAME_QUIT = True # Not working
+                pygame.quit()
+                quit()
+                sys.exit(0) # safe backup   
+
+        #NOT USED
         #if event.type == pygame.KEYUP:
             #if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 #dx = 0
             #if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 #dy = 0
+
+            #break is ot make sure that while the game is "paused" or "waiting" for the clock fps cycle to finish (since its a low fps and the player can easily push multiple
+            #commands each frame) the game only reads in the first command each frame (can make it the last, buts thats an effort)
             break
 
     return GAME_OVER
 
+
+#Main game loop
 def gameLoop(GAME_QUIT):
+
+    #initialize starting variable to ensure that the players can retry after a GAME OVER
     score = 0
-    snake.dx = MOVEMENT_SPEED
+    snake.dx = MOVEMENT_SPEED # Initilise the snake moving to the left
     snake.dy = 0
-    snake.x = 0
+    snake.x = 0 # Snake in top left corner
     snake.y = 0
     GAME_OVER = False
 
-    print("In game loop")
+    #print("In game loop") # DEBUGGING
 
     while not GAME_QUIT:
-        #print("within the game loop")
+        #print("Within the game loop") # DEBUGGING
+
+        #Players controls while in the game loop
         GAME_OVER = controls(GAME_OVER)
 
         #blue_rect.topleft = pygame.mouse.get_pos()
 
         if GAME_OVER:
+            #Game over screen
             gameOver() 
         else:
+            #In game screen
             FPS_text = myfont.render("FPS: %0.3f" % clock.get_fps(), True, white)  # anti aliasing lowers performance
             score_text = myfont.render("Score: " + str(score), True, white)
             
+            #Checks if the snake has eaten the food
             eatsFood(score)
 
+            #Update the snakes positioning
             #gameUpdate()
             snake.update(SCALE)
             
-            if snake.box[0].topleft != (snake.x, snake.y):
+            #Update the snakes tail positions (from back to front)
+            if snake.box[0].topleft != (snake.x, snake.y): # if the snake has moved
 
-                if len(snake.tail) >= 1:
-                    for x in range(len(snake.tail) - 1, 0, -1):
-                        #print(x)
-                        snake.box[x].topleft = snake.box[x-1].topleft
+                if snake.tail_length > 0:
+                    for i in range(snake.tail_length, 0, -1):
+                        #print(i)
+                        snake.box[i].topleft = snake.box[i-1].topleft
 
                 if ENABLE_WRAP:
                     wrap(snake.x, snake.y)
 
+                #Update the head position of the snake
                 snake.box[0].topleft = (snake.x, snake.y)
 
-            #if (red_mask.overlap(snake.mask[0], (0, 0)) != None):
-
-                if len(snake.tail) > 1:
-                    for i in range(1, len(snake.tail)-1, 1):
-                        #print(range(1, len(snake.tail)-1, 1))
+                #If you think about it, the snake can only start crashing into itself after the tail length it greater than 3
+                if snake.tail_length >= 3:
+                    for i in range(1, snake.tail_length):
                         if(snake.box[0].colliderect(snake.box[i])):
                             GAME_OVER = True
-                            print("Game Over")
-                            print("Try again?")
-                            #GAME_OVER = gameOverLoop()
-
-            #DUPLICATE CODE FOR DEBUGGING                
-            if False:
-                if snake.tail_length > 1:
-                    for i in range(snake.tail_length - 1, 0, -1):
-                        #print(x)
-                        snake.tail_box[i].topleft = snake.tail_box[i-1].topleft
-
-                if ENABLE_WRAP:
-                    wrap(snake.x, snake.y)
-
-                snake.tail_box[0].topleft = (snake.x, snake.y)
-
-                snake.update(SCALE)
-
-                snake.head_box.topleft = (snake.x, snake.y)
-
-                if len(snake.tail_length) > 1:
-                    for i in range(1, snake.tail_length-1, 1):
-                        #print(range(1, len(snake.tail)-1, 1))
-                        if(snake.box[0].colliderect(snake.box[i])):
-                            GAME_OVER = True
-                            print("Game Over")
-                            print("Try again?")
-                            #GAME_OVER = gameOverLoop()
+                            #DEBUGGING
+                            #print("Game Over")
+                            #print("Try again?")
 
             gameDisplay.fill(black)  #set background
             gameDisplay.blit(FPS_text, (0, 0))
-            gameDisplay.blit(score_text, (DISPLAY_WIDTH -150, 0))
+            gameDisplay.blit(score_text, (DISPLAY_WIDTH - 150, 0))
 
             DrawBlueDot()
             DrawSnake()
@@ -317,25 +297,30 @@ def gameLoop(GAME_QUIT):
         #print(clock.get_rawtime())
         clock.tick(FPS) # FPS setting
         #print(GAME_OVER)
-                
+
+    #Outside of Main Game While Loop i.e if GAME_QUIT == True
+    pygame.quit()
+    quit()
+    sys.exit(0) # safe backup   
         
 
 def gameOver():
 
     gameDisplay.fill(black)  #set background
     game_over_text = gameOverFont.render("GAME OVER", True, white)
-    try_again_text = myfont.render("Press SPACE to try again", True, white)
-    gameDisplay.blit(game_over_text, (200, 200))
-    gameDisplay.blit(try_again_text, (200, 400))
+    try_again_text = myfont.render("Press SPACE to Try Again", True, white)
+    quit_text = myfont.render("Press Q to Quit", True, white)
+
+    gameDisplay.blit(game_over_text, (120, 200))
+    gameDisplay.blit(try_again_text, (235, 400))
+    gameDisplay.blit(quit_text, (300, 450))
 
     snake.dx = MOVEMENT_SPEED
     snake.dy = 0
     snake.x = 0
     snake.y = 0
 
-    #del snake.mask[:]
-
-snake.box[0].topleft = (snake.tail[0][0], snake.tail[0][1])
+snake.box[0].topleft = (snake.x, snake.y)
 makeFood()
 
 GAME_QUIT = False
--- Tail.py
+++ Tail.py
@@ -5,19 +5,18 @@ class Snake:
         self.y = y
         self.dx = dx
         self.dy = dy
+        
         self.head_img = None
-        self.head_mask = None
-        self.head_box = None
+        self.head_mask = None # used for collision with food
         self.tail_img = None
-        self.tail_box = [] # List of Rectangles tuples (x,y,width,height) for the snakes tail
-        self.tail = [] # Tail positions
-        self.mask = []
-        self.box = [] # Tail rectangles used for collision
-        self.tail_length = 0
+
+        self.box = [] # List of Rectangles tuples (x,y,width,height) for the snakes tail --> The main one
+        self.tail_length = 0 # basically len(snake.box) - 1
 
     def update(self, scale):
         self.x += self.dx * scale
         self.y += self.dy * scale
+        #DEBUGGING
         #print("SNAKE POSITION UPDATED")
         #print("dx = " + str(self.dx))
         #print("dy = " + str(self.dy))
