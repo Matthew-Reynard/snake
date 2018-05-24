ff20a0e744a4cad723cf38d900e013417f6722e9
Matthew Reynard <matthewreynard24@gmail.com>
2018-05-24 15:16:48 +0200

    Networking update

---
+++ sockets.py
@@ -0,0 +1,99 @@
+import socket
+import sys
+# from _thread import *
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP connection
+
+print(s)
+
+# server = "127.0.0.1"
+server = "localhost"
+host = ""
+# server = "www.matthewreynard.com"
+port = 5555
+
+# CLIENT
+
+s.connect((server, port))
+
+while True:
+
+	data = input("Send (q to Quit): ")
+	if (data != "Q" and data != "q"):
+		if(data=="p"):
+			s.send(str.encode(data + "\n"))
+			print("P was sent")
+			# while True:
+			r = s.recv(1024)
+
+			state = "Server message: "+ r.decode("utf-8")
+				# if not r:
+				# 	break
+			print(state)	
+		else:
+			s.send(str.encode(data + "\n"))
+	else:
+		s.send(str.encode(data + "\n"))
+		s.close()
+		break
+
+
+
+
+
+
+# server_ip = socket.gethostbyname(server)
+# print(server_ip)
+
+# request = "GET / HTTP/1.1\nHost: " + server + "\n\n"
+
+# s.connect((server, port))
+# s.send(request.encode()) #encoding into byte format
+
+# result = s.recv(1024)
+
+# print(result)
+
+# def pscan(port):
+# 	try:
+# 		s.connect((server,port))
+# 		return True
+# 	except:
+# 		return False
+
+
+# for x in range (1, 10000):
+# 	if pscan(x):
+# 		print("Port",x,"is open")
+# 	else:
+# 		print("Port",x,"is closed")
+
+# try:
+# 	s.bind((host, port))
+# except socket.error as e:
+# 	print(str(e))
+
+# s.listen(5)
+
+# print("Waiting for connection...")
+
+# def threaded_client(conn):
+# 	conn.send(str.encode("Welcome, enter a number:\n"))
+
+# 	while True:
+# 		data = conn.recv(1024)
+# 		reply = "Server output: "+ data.decode("utf-8")
+# 		if not data:
+# 			break
+# 		conn.sendall(str.encode(reply))
+
+# 	conn.close()
+
+
+# while True:
+
+# 	conn, addr = s.accept()
+# 	print("connected to: "+addr[0]+":"+str(addr[1]))
+
+# 	start_new_thread(threaded_client,(conn,))
+
