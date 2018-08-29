02a504a818a2d97558f12974f307a77727b77555
Matthew Reynard <matthewreynard24@gmail.com>
2018-08-29 17:09:25 +0200

    Setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setuptools
+
+setup(
+	name="test",
+	version="1.0",
+	description="Snake game",
+	author="Matthew Reynard",
+	author_email="matthewreynard24@gmail.com",
+	url="matthewreyanrd.com",
+	packages=["snake"],
+	py_modules=["test"]
+	install_requires=[
+	"test",
+	"sockets",
+	]
+)
