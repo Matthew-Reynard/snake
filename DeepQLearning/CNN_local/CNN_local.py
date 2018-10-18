0fba4433a0ba5214de78f750b32d028f1e586ab0
Matthew-Reynard <matthewreynard24@gmail.com>
2018-10-18 23:32:44 +0200

    Updates

--- DeepQLearning/CNN_local/CNN_local.py
+++ DeepQLearning/CNN_local/CNN_local.py
@@ -192,9 +192,9 @@ def trainDeepModel(load = False):
 	epsilon = 0.1  # Probability to choose random action instead of best action
 
 	epsilon_function = True
-	epsilon_start = 0.5
-	epsilon_end = 0.1
-	epsilon_percentage = 0.6 # in decimal
+	epsilon_start = 0.15
+	epsilon_end = 0.05
+	epsilon_percentage = 0.5 # in decimal
 
 	alpha_function = False
 	alpha_start = 0.01
@@ -241,7 +241,7 @@ def trainDeepModel(load = False):
 	# errors = []
 
 	print_episode = 1000
-	total_episodes = 100000
+	total_episodes = 1000000
 
 	# Saving model capabilities
 	saver = tf.train.Saver()
